6d4a69a974a58f32e4faccbdca45266179f58b91
Sean Ferguson <scferg11@gmail.com>
2018-06-18 01:27:46 -0400

    v1.0.0

--- disable-password-change-admin-email.php
+++ disable-password-change-admin-email.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Disable Password Change Admin Email
-Description: Whatever
+Description: Disable emails sent to admins when a user changes their password
 Version: 1.0.0
 Author: Sean Ferguson
 Author URI: https://github.com/scferg/
@@ -9,7 +9,6 @@ License: GPL v3
 */
 
 /**
- * Disable emails sent to admins when a user changes their password
  * Credit: https://wordpress.stackexchange.com/a/266006/48662
  */
  if (!function_exists('wp_password_change_notification')) {
